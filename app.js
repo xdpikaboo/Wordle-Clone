167d6415b6956f797e0335396908cfa4b86fdf4d
xdpikaboo <xdpikaboo@gmail.com>
2022-02-22 17:45:05 -0500

    Revert "Revert "Hide API KEYs""
    This reverts commit 5a48265d5446e322ab149ce55ea6d80e06369481.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const messageDisplay = document.querySelector(".message-container");
 
 let isGameOver = false;
 let wordle;
+var key = config.SECRET_API_KEY;
 
 const getWordle = () => {
   fetch(
@@ -12,7 +13,7 @@ const getWordle = () => {
       method: "GET",
       headers: {
         "x-rapidapi-host": "random-words5.p.rapidapi.com",
-        "x-rapidapi-key": "82d3059513msh2cf51c4b98b492dp1aa518jsnb06dd1b8449b",
+        "x-rapidapi-key": key,
       },
     }
   )
@@ -140,8 +141,7 @@ const checkRow = () => {
         method: "GET",
         headers: {
           "x-rapidapi-host": "twinword-word-graph-dictionary.p.rapidapi.com",
-          "x-rapidapi-key":
-            "82d3059513msh2cf51c4b98b492dp1aa518jsnb06dd1b8449b",
+          "x-rapidapi-key": key,
         },
       }
     )
@@ -159,7 +159,7 @@ const checkRow = () => {
             return;
           } else {
             if (currentRow >= 5) {
-              showMessage("U Suck!");
+              showMessage("Game Over! Wordle is " + wordle);
               isGameOver = true;
               return;
             }
