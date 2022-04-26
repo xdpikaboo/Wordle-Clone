d0619d6c75dd5ab7de18e9480f1cd59bf4a6cfde
xdpikaboo <xdpikaboo@gmail.com>
2022-04-25 21:46:58 -0400

    New MODES!

--- app.js
+++ app.js
@@ -1,30 +1,14 @@
-const tileDisplay = document.querySelector(".tile-container");
+let tileDisplay = document.querySelector(".tile-container");
+const wordsDisplay = document.querySelector(".words-container");
+const buttons = document.querySelector(".button-container");
 const keyboard = document.querySelector(".key-container");
 const messageDisplay = document.querySelector(".message-container");
 
 let isGameOver = false;
 let wordle;
-
-const getWordle = () => {
-  fetch(
-    "https://random-words5.p.rapidapi.com/getMultipleRandom?count=1&wordLength=5",
-    {
-      method: "GET",
-      headers: {
-        "x-rapidapi-host": "random-words5.p.rapidapi.com",
-        "x-rapidapi-key": "82d3059513msh2cf51c4b98b492dp1aa518jsnb06dd1b8449b",
-      },
-    }
-  )
-    .then((response) => response.json())
-    .then((json) => {
-      console.log(json);
-      wordle = json[0].toUpperCase();
-    })
-    .catch((error) => console.log(error));
-};
-getWordle();
-
+let size = 5;
+let line;
+let tiles;
 const keys = [
   "Q",
   "W",
@@ -56,37 +40,148 @@ const keys = [
   "<<",
 ];
 
-const tiles = [
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-];
+const getWordle = () => {
+  fetch(
+    "https://random-words5.p.rapidapi.com/getMultipleRandom?count=1&wordLength=" +
+      size,
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "random-words5.p.rapidapi.com",
+        "x-rapidapi-key": "82d3059513msh2cf51c4b98b492dp1aa518jsnb06dd1b8449b",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      wordle = json[0].toUpperCase();
+    })
+    .catch((error) => console.log(error));
+};
+getWordle();
 
 let currentRow = 0;
 let currentTile = 0;
 
-keys.forEach((key) => {
-  const buttonElement = document.createElement("button");
-  buttonElement.textContent = key;
-  buttonElement.setAttribute("id", key);
-  buttonElement.addEventListener("click", () => handleClick(key));
-  keyboard.append(buttonElement);
-});
+const inc = document.createElement("button");
+inc.textContent = "+";
+inc.addEventListener("click", () => increment());
+buttons.append(inc);
 
-tiles.forEach((row, rowIndex) => {
-  const rowElement = document.createElement("div");
-  rowElement.setAttribute("id", "row-" + rowIndex);
-  row.forEach((tile, tileIndex) => {
-    const tileElement = document.createElement("div");
-    tileElement.setAttribute("id", "row-" + rowIndex + "-tile-" + tileIndex);
-    tileElement.classList.add("tile");
-    rowElement.append(tileElement);
+const dec = document.createElement("button");
+dec.textContent = "-";
+dec.addEventListener("click", () => decrement());
+buttons.append(dec);
+
+const reset = document.createElement("button");
+reset.textContent = "New game";
+reset.addEventListener("click", () => resetGame());
+buttons.append(reset);
+
+const hard = document.createElement("button");
+hard.textContent = "This is too ez~";
+hard.addEventListener("click", () => (window.location.href = "hard.html"));
+buttons.append(hard);
+
+const render = () => {
+  line = [];
+  tiles = [];
+  for (let i = 0; i < size; i++) {
+    line.push("");
+  }
+  for (let i = 0; i < 6; i++) {
+    tiles.push(line);
+  }
+  tiles.forEach((row, rowIndex) => {
+    const rowElement = document.createElement("div");
+    rowElement.setAttribute("id", "row-" + rowIndex);
+    row.forEach((tile, tileIndex) => {
+      const tileElement = document.createElement("div");
+      tileElement.setAttribute("id", "row-" + rowIndex + "-tile-" + tileIndex);
+      tileElement.classList.add("tile");
+      rowElement.append(tileElement);
+    });
+    tileDisplay.append(rowElement);
   });
-  tileDisplay.append(rowElement);
-});
+  console.log(tiles);
+  keys.forEach((key) => {
+    const buttonElement = document.createElement("button");
+    buttonElement.textContent = key;
+    buttonElement.setAttribute("id", key);
+    buttonElement.addEventListener("click", () => handleClick(key));
+    keyboard.append(buttonElement);
+  });
+};
+
+render();
+
+const increment = () => {
+  if (size == 10) {
+    showMessage("Maximum wordle size is 10!");
+  } else {
+    size++;
+    getWordle();
+    const tiles = document.querySelectorAll(".tile-container");
+    tiles.forEach((tile) => {
+      tile.remove();
+    });
+    tileDisplay = document.createElement("div");
+    tileDisplay.classList.add("tile-container");
+    wordsDisplay.append(tileDisplay);
+    const buttons = document.querySelectorAll(".key-container button");
+    buttons.forEach((button) => {
+      button.remove();
+    });
+    currentRow = 0;
+    currentTile = 0;
+    isGameOver = false;
+    render();
+  }
+};
+
+const decrement = () => {
+  if (size == 4) {
+    showMessage("Minimum wordle size is 4!");
+  } else {
+    size--;
+    getWordle();
+    const tiles = document.querySelectorAll(".tile-container");
+    tiles.forEach((tile) => {
+      tile.remove();
+    });
+    tileDisplay = document.createElement("div");
+    tileDisplay.classList.add("tile-container");
+    wordsDisplay.append(tileDisplay);
+    const buttons = document.querySelectorAll(".key-container button");
+    buttons.forEach((button) => {
+      button.remove();
+    });
+    currentRow = 0;
+    currentTile = 0;
+    isGameOver = false;
+    render();
+  }
+};
+
+const resetGame = () => {
+  getWordle();
+  const tiles = document.querySelectorAll(".tile-container");
+  tiles.forEach((tile) => {
+    tile.remove();
+  });
+  tileDisplay = document.createElement("div");
+  tileDisplay.classList.add("tile-container");
+  wordsDisplay.append(tileDisplay);
+  const buttons = document.querySelectorAll(".key-container button");
+  buttons.forEach((button) => {
+    button.remove();
+  });
+  currentRow = 0;
+  currentTile = 0;
+  isGameOver = false;
+  render();
+};
 
 const handleClick = (key) => {
   if (!isGameOver) {
@@ -104,7 +199,7 @@ const handleClick = (key) => {
 };
 
 const addLetter = (letter) => {
-  if (currentTile < 5 && currentRow < 6 && isGameOver == false) {
+  if (currentTile < size && currentRow < 6 && isGameOver == false) {
     const tile = document.getElementById(
       "row-" + currentRow + "-tile-" + currentTile
     );
@@ -112,7 +207,6 @@ const addLetter = (letter) => {
     tiles[currentRow][currentTile] = letter;
     tile.setAttribute("data", letter);
     currentTile++;
-    console.log(tiles);
   }
 };
 
@@ -130,9 +224,12 @@ const deleteLetter = () => {
 };
 
 const checkRow = () => {
+  if (currentRow >= 5) {
+    showMessage("Game Over! Word is " + wordle);
+  }
   const guess = tiles[currentRow].join("");
   console.log("guess", guess);
-  if (currentTile > 4 && isGameOver == false) {
+  if (currentTile > size - 1 && isGameOver == false) {
     fetch(
       "https://twinword-word-graph-dictionary.p.rapidapi.com/association/?entry=" +
         guess,
@@ -190,33 +287,29 @@ const flipTile = () => {
   const rowTiles = document.querySelector("#row-" + currentRow).childNodes;
   let checkWordle = wordle;
   const guess = [];
+  rowTiles.forEach((tile) => {
+    guess.push({ letter: tile.getAttribute("data"), color: "grey-overlay" });
+  });
+
+  guess.forEach((guess, index) => {
+    if (guess.letter == wordle[index]) {
+      guess.color = "green-overlay";
+      checkWordle = checkWordle.replace(guess.letter, "");
+    }
+  });
+
+  guess.forEach((guess) => {
+    if (checkWordle.includes(guess.letter)) {
+      guess.color = "yellow-overlay";
+      checkWordle = checkWordle.replace(guess.letter, "");
+    }
+  });
+
   rowTiles.forEach((tile, index) => {
-    const dataLetter = tile.getAttribute("data");
-
-    rowTiles.forEach((tile) => {
-      guess.push({ letter: tile.getAttribute("data"), color: "grey-overlay" });
-    });
-
-    guess.forEach((guess, index) => {
-      if (guess.letter == wordle[index]) {
-        guess.color = "green-overlay";
-        checkWordle = checkWordle.replace(guess.letter, "");
-      }
-    });
-
-    guess.forEach((guess) => {
-      if (checkWordle.includes(guess.letter)) {
-        guess.color = "yellow-overlay";
-        checkWordle = checkWordle.replace(guess.letter, "");
-      }
-    });
-
-    rowTiles.forEach((tile, index) => {
-      setTimeout(() => {
-        tile.classList.add("flip");
-        tile.classList.add(guess[index].color);
-        addColorToKey(guess[index].letter, guess[index].color);
-      }, 375 * index);
-    });
+    setTimeout(() => {
+      tile.classList.add("flip");
+      tile.classList.add(guess[index].color);
+      addColorToKey(guess[index].letter, guess[index].color);
+    }, 375 * index);
   });
 };
---
+++ hard.js
@@ -0,0 +1,491 @@
+const wordsDisplay = document.querySelector(".words-container");
+const buttons = document.querySelector(".button-container");
+const keyboard = document.querySelector(".key-container");
+const messageDisplay = document.querySelector(".message-container");
+
+let isGameOver1 = false;
+let isGameOver2 = false;
+let isGameOver3 = false;
+let wordle1;
+let wordle2;
+let wordle3;
+let line;
+let tiles;
+let words;
+let size = 5;
+const keys = [
+  "Q",
+  "W",
+  "E",
+  "R",
+  "T",
+  "Y",
+  "U",
+  "I",
+  "O",
+  "P",
+  "A",
+  "S",
+  "D",
+  "F",
+  "G",
+  "H",
+  "J",
+  "K",
+  "L",
+  "ENTER",
+  "Z",
+  "X",
+  "C",
+  "V",
+  "B",
+  "N",
+  "M",
+  "<<",
+];
+
+const getWordle = () => {
+  fetch(
+    "https://random-words5.p.rapidapi.com/getMultipleRandom?count=1&wordLength=" +
+      size,
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "random-words5.p.rapidapi.com",
+        "x-rapidapi-key": "82d3059513msh2cf51c4b98b492dp1aa518jsnb06dd1b8449b",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      wordle1 = json[0].toUpperCase();
+      console.log(wordle1);
+    })
+    .catch((error) => console.log(error));
+  fetch(
+    "https://random-words5.p.rapidapi.com/getMultipleRandom?count=1&wordLength=" +
+      size,
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "random-words5.p.rapidapi.com",
+        "x-rapidapi-key": "82d3059513msh2cf51c4b98b492dp1aa518jsnb06dd1b8449b",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      wordle2 = json[0].toUpperCase();
+      console.log(wordle2);
+    })
+    .catch((error) => console.log(error));
+  fetch(
+    "https://random-words5.p.rapidapi.com/getMultipleRandom?count=1&wordLength=" +
+      size,
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "random-words5.p.rapidapi.com",
+        "x-rapidapi-key": "82d3059513msh2cf51c4b98b492dp1aa518jsnb06dd1b8449b",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      wordle3 = json[0].toUpperCase();
+      console.log(wordle3);
+    })
+    .catch((error) => console.log(error));
+};
+getWordle();
+
+let currentRow = 0;
+let currentTile = 0;
+
+keys.forEach((key) => {
+  const buttonElement = document.createElement("button");
+  buttonElement.textContent = key;
+  buttonElement.addEventListener("click", () => handleClick(key));
+  keyboard.append(buttonElement);
+});
+
+const inc = document.createElement("button");
+inc.textContent = "+";
+inc.addEventListener("click", () => increment());
+buttons.append(inc);
+
+const dec = document.createElement("button");
+dec.textContent = "-";
+dec.addEventListener("click", () => decrement());
+buttons.append(dec);
+
+const reset = document.createElement("button");
+reset.textContent = "New game";
+reset.addEventListener("click", () => resetGame());
+buttons.append(reset);
+
+const easy = document.createElement("button");
+easy.textContent = "This is too hard!";
+easy.addEventListener("click", () => (window.location.href = "index.html"));
+buttons.append(easy);
+
+const harder = document.createElement("button");
+harder.textContent = "Still too ez~";
+harder.addEventListener("click", () => (window.location.href = "harder.html"));
+buttons.append(harder);
+
+const render = () => {
+  words = [];
+  line = [];
+  tiles = [];
+  for (let i = 0; i < size; i++) {
+    line.push("");
+  }
+  for (let i = 0; i < 6; i++) {
+    tiles.push(line);
+  }
+  for (let i = 0; i < 3; i++) {
+    words.push(tiles);
+  }
+  words.forEach((tiles, wordIndex) => {
+    const wordElement = document.createElement("div");
+    wordElement.setAttribute("id", "word-" + wordIndex);
+    wordElement.classList.add("tile-container");
+    tiles.forEach((row, rowIndex) => {
+      const rowElement = document.createElement("div");
+      rowElement.setAttribute("id", "word-" + wordIndex + "-row-" + rowIndex);
+      row.forEach((tile, tileIndex) => {
+        const tileElement = document.createElement("div");
+        tileElement.setAttribute(
+          "id",
+          "word-" + wordIndex + "-row-" + rowIndex + "-tile-" + tileIndex
+        );
+        tileElement.classList.add("tile");
+        rowElement.append(tileElement);
+      });
+      wordElement.append(rowElement);
+    });
+    wordsDisplay.append(wordElement);
+  });
+};
+render();
+
+const increment = () => {
+  if (size == 10) {
+    showMessage("Maximum wordle size is 10!");
+  } else {
+    size++;
+    getWordle();
+    const tiles = document.querySelectorAll(".tile-container");
+    tiles.forEach((tile) => {
+      tile.remove();
+    });
+    currentRow = 0;
+    currentTile = 0;
+    isGameOver1 = false;
+    isGameOver2 = false;
+    isGameOver3 = false;
+    render();
+  }
+};
+
+const decrement = () => {
+  if (size == 4) {
+    showMessage("Minimum wordle size is 4!");
+  } else {
+    size--;
+    getWordle();
+    const tiles = document.querySelectorAll(".tile-container");
+    tiles.forEach((tile) => {
+      tile.remove();
+    });
+    currentRow = 0;
+    currentTile = 0;
+    isGameOver1 = false;
+    isGameOver2 = false;
+    isGameOver3 = false;
+    render();
+  }
+};
+
+const resetGame = () => {
+  getWordle();
+  const tiles = document.querySelectorAll(".tile-container");
+  tiles.forEach((tile) => {
+    tile.remove();
+  });
+  currentRow = 0;
+  currentTile = 0;
+  isGameOver1 = false;
+  isGameOver2 = false;
+  isGameOver3 = false;
+  render();
+};
+
+const handleClick = (key) => {
+  if (!isGameOver1 || !isGameOver2 || !isGameOver3) {
+    console.log("clicked", key);
+    if (key == "<<") {
+      deleteLetter();
+      return;
+    }
+    if (key == "ENTER") {
+      checkRow();
+      return;
+    }
+    addLetter(key);
+  }
+};
+
+const addLetter = (letter) => {
+  if (currentTile < size && currentRow < 6) {
+    if (!isGameOver1) {
+      const tile1 = document.getElementById(
+        "word-0" + "-row-" + currentRow + "-tile-" + currentTile
+      );
+      tile1.textContent = letter;
+      words[0][currentRow][currentTile] = letter;
+      tile1.setAttribute("data", letter);
+    }
+    if (!isGameOver2) {
+      const tile2 = document.getElementById(
+        "word-1" + "-row-" + currentRow + "-tile-" + currentTile
+      );
+      tile2.textContent = letter;
+      words[1][currentRow][currentTile] = letter;
+      tile2.setAttribute("data", letter);
+    }
+    if (!isGameOver3) {
+      const tile3 = document.getElementById(
+        "word-2" + "-row-" + currentRow + "-tile-" + currentTile
+      );
+      tile3.textContent = letter;
+      words[2][currentRow][currentTile] = letter;
+      tile3.setAttribute("data", letter);
+    }
+    if (!isGameOver1 || !isGameOver2 || !isGameOver3) {
+      currentTile++;
+    }
+  }
+};
+
+const deleteLetter = () => {
+  if (currentTile > 0) {
+    if (!isGameOver1 || !isGameOver2 || !isGameOver3) {
+      currentTile--;
+    }
+    if (!isGameOver1) {
+      const tile1 = document.getElementById(
+        "word-0" + "-row-" + currentRow + "-tile-" + currentTile
+      );
+      tile1.textContent = "";
+      words[0][currentRow][currentTile] = "";
+      tile1.setAttribute("data", "");
+    }
+    if (!isGameOver2) {
+      const tile2 = document.getElementById(
+        "word-1" + "-row-" + currentRow + "-tile-" + currentTile
+      );
+      tile2.textContent = "";
+      words[1][currentRow][currentTile] = "";
+      tile2.setAttribute("data", "");
+    }
+    if (!isGameOver3) {
+      const tile3 = document.getElementById(
+        "word-2" + "-row-" + currentRow + "-tile-" + currentTile
+      );
+      tile3.textContent = "";
+      words[2][currentRow][currentTile] = "";
+      tile3.setAttribute("data", "");
+    }
+  }
+};
+
+const checkRow = () => {
+  if (currentRow >= 5) {
+    showMessage(
+      "Game Over! Words are " + wordle1 + "," + wordle2 + "," + wordle3
+    );
+  }
+  const guess = words[0][currentRow].join("");
+  console.log("guess", guess);
+  if (
+    currentTile > size - 1 &&
+    (!isGameOver1 || !isGameOver2 || !isGameOver3)
+  ) {
+    fetch(
+      "https://twinword-word-graph-dictionary.p.rapidapi.com/association/?entry=" +
+        guess,
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "twinword-word-graph-dictionary.p.rapidapi.com",
+          "x-rapidapi-key":
+            "82d3059513msh2cf51c4b98b492dp1aa518jsnb06dd1b8449b",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        if (json.result_msg == "Entry word not found") {
+          showMessage("Not a Word!");
+          return;
+        } else {
+          flipTile();
+          if (wordle1 == guess) {
+            showMessage("Good Job!");
+            isGameOver1 = true;
+            if (currentRow < 5) {
+              currentRow++;
+              currentTile = 0;
+            }
+            return;
+          } else if (wordle2 == guess) {
+            showMessage("Good Job!");
+            isGameOver2 = true;
+            if (currentRow < 5) {
+              currentRow++;
+              currentTile = 0;
+            }
+            return;
+          } else if (wordle3 == guess) {
+            showMessage("Good Job!");
+            isGameOver3 = true;
+            if (currentRow < 5) {
+              currentRow++;
+              currentTile = 0;
+            }
+            return;
+          } else {
+            if (currentRow >= 5) {
+              showMessage(
+                "Game Over! Words are " +
+                  wordle1 +
+                  "," +
+                  wordle2 +
+                  "," +
+                  wordle3
+              );
+              isGameOver1 = true;
+              isGameOver2 = true;
+              isGameOver3 = true;
+              return;
+            }
+            if (currentRow < 5) {
+              currentRow++;
+              currentTile = 0;
+            }
+          }
+        }
+      })
+      .catch((error) => console.log(error));
+  }
+  console.log(isGameOver1);
+  console.log(isGameOver2);
+  console.log(isGameOver3);
+};
+
+const addColorToKey = (keyLetter, color) => {
+  const key = document.getElementById(keyLetter);
+  key.classList.add(color);
+};
+
+const showMessage = (message) => {
+  const messageElement = document.createElement("p");
+  messageElement.textContent = message;
+  messageDisplay.append(messageElement);
+  setTimeout(() => messageDisplay.removeChild(messageElement), 2000);
+};
+
+const flipTile = () => {
+  if (!isGameOver1) {
+    const rowTiles1 = document.querySelector(
+      "#word-" + 0 + "-row-" + currentRow
+    ).childNodes;
+    let checkWordle1 = wordle1;
+    const guess1 = [];
+    rowTiles1.forEach((tile) => {
+      guess1.push({ letter: tile.getAttribute("data"), color: "grey-overlay" });
+    });
+    guess1.forEach((guess, index) => {
+      if (guess.letter == wordle1[index]) {
+        guess.color = "green-overlay";
+        checkWordle1 = checkWordle1.replace(guess.letter, "");
+      }
+    });
+    guess1.forEach((guess) => {
+      if (checkWordle1.includes(guess.letter)) {
+        guess.color = "yellow-overlay";
+        checkWordle1 = checkWordle1.replace(guess.letter, "");
+      }
+    });
+    rowTiles1.forEach((tile, index) => {
+      setTimeout(() => {
+        tile.classList.add("flip");
+        tile.classList.add(guess1[index].color);
+        addColorToKey(guess1[index].letter, guess1[index].color);
+      }, 375 * index);
+    });
+  }
+
+  if (!isGameOver2) {
+    const rowTiles2 = document.querySelector(
+      "#word-" + 1 + "-row-" + currentRow
+    ).childNodes;
+    let checkWordle2 = wordle2;
+    const guess2 = [];
+    rowTiles2.forEach((tile) => {
+      guess2.push({ letter: tile.getAttribute("data"), color: "grey-overlay" });
+    });
+    guess2.forEach((guess, index) => {
+      if (guess.letter == wordle2[index]) {
+        guess.color = "green-overlay";
+        checkWordle2 = checkWordle2.replace(guess.letter, "");
+      }
+    });
+    guess2.forEach((guess) => {
+      if (checkWordle2.includes(guess.letter)) {
+        guess.color = "yellow-overlay";
+        checkWordle2 = checkWordle2.replace(guess.letter, "");
+      }
+    });
+    rowTiles2.forEach((tile, index) => {
+      setTimeout(() => {
+        tile.classList.add("flip");
+        tile.classList.add(guess2[index].color);
+        addColorToKey(guess2[index].letter, guess2[index].color);
+      }, 375 * index);
+    });
+  }
+
+  if (!isGameOver3) {
+    const rowTiles3 = document.querySelector(
+      "#word-" + 2 + "-row-" + currentRow
+    ).childNodes;
+    let checkWordle3 = wordle3;
+    const guess3 = [];
+    rowTiles3.forEach((tile) => {
+      guess3.push({ letter: tile.getAttribute("data"), color: "grey-overlay" });
+    });
+    guess3.forEach((guess, index) => {
+      if (guess.letter == wordle3[index]) {
+        guess.color = "green-overlay";
+        checkWordle3 = checkWordle3.replace(guess.letter, "");
+      }
+    });
+    guess3.forEach((guess) => {
+      if (checkWordle3.includes(guess.letter)) {
+        guess.color = "yellow-overlay";
+        checkWordle3 = checkWordle3.replace(guess.letter, "");
+      }
+    });
+    rowTiles3.forEach((tile, index) => {
+      setTimeout(() => {
+        tile.classList.add("flip");
+        tile.classList.add(guess3[index].color);
+        addColorToKey(guess3[index].letter, guess3[index].color);
+      }, 375 * index);
+    });
+  }
+};
